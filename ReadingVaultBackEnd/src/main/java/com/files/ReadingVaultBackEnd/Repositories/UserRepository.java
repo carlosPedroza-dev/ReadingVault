6f86b7baf25c0297c0357ef2a2b68d5e68c52e34
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-12-03 17:39:19 -0700

    add JPA custom methods to UserRepository(findbyid,existbyemail,findbyPassword)

--- ReadingVaultBackEnd/src/main/java/com/files/ReadingVaultBackEnd/Repositories/UserRepository.java
+++ ReadingVaultBackEnd/src/main/java/com/files/ReadingVaultBackEnd/Repositories/UserRepository.java
@@ -1,10 +1,16 @@
 package com.files.ReadingVaultBackEnd.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.files.ReadingVaultBackEnd.Entities.User;
+
+
 @Repository
 interface  UserRepository extends JpaRepository<User,Long>{
-    
+    Optional<User>findByid(Long id);
+    boolean existByemail(String email);
+    Optional<User> findByPassword(String password);
 }
