de4152640196a85e750ac37394550ee9802e6fff
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-12-03 17:22:31 -0700

    add setters and getters for User

--- ReadingVaultBackEnd/src/main/java/com/files/ReadingVaultBackEnd/Entities/User.java
+++ ReadingVaultBackEnd/src/main/java/com/files/ReadingVaultBackEnd/Entities/User.java
@@ -12,12 +12,44 @@ import jakarta.persistence.Table;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
+   private Long id;
 
     @Column(name="user_email",unique=true)
-    String email;
+   private String email;
 
-    @Column(name="user_password",unique=true)
-    String password;
+    @Column(name="user_password")
+   private String password;
+    
+    public User(){}
+
+    public User(String email,String password){
+        this.email = email;
+        this.password = password;
+    }
+
+public void setId(Long id){
+    this.id = id;
+}
+
+public Long getId(){
+    return id;
+}
+
+   public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+public String getEmail(){
+    return email;
+}
+
+    public String getPassword(){
+        return password;
+    }
+
     
 }
