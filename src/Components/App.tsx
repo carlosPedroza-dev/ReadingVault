279404665f3a929fded456c0f71fe2a0ef4a6aed
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-12-22 20:07:36 -0700

    adjust frontend files and create vertical navbar template

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,6 +2,8 @@ import VNav from './navbar';
 
 export default function App(){
     return(
+        <>
 <VNav/>
+</>
     );
 }
--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -2,8 +2,13 @@ import '../css/globalcss.css';
 
 export default function VerticalNavbar(){
     return(
-<div className='bg'>
-
-</div>
+<nav className='bg-teal-500 h-screen w-64'>
+    <ul className='flex flex-col gap-2 p-4'>
+        <li><a className='block rounded' href="">Profile</a></li>
+        <li><a href="">Books</a></li>
+        <li><a href="">Mangas</a></li>
+        <li><a href="">Comics</a></li>
+    </ul>
+</nav>
     );
 }
