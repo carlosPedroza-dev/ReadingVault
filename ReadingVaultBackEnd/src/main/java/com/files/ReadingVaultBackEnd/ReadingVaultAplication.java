51f9d3de190fa99f73644cff529fac3d42408003
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2026-01-07 16:04:40 -0700

    upload a example application.properties

---
+++ ReadingVaultBackEnd/src/main/java/com/files/ReadingVaultBackEnd/ReadingVaultAplication.java
@@ -0,0 +1,10 @@
+package com.files.ReadingVaultBackEnd;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class ReadingVaultAplication {
+    public static void main(String[] args){
+        SpringApplication.run(ReadingVaultAplication.class,args);
+    }
+}
